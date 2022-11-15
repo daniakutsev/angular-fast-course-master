bb48c2643e415f1948505cc6538200fe43c4782d
daniakutsev <daniakutsev1488@gmail.com>
2022-11-15 17:52:54 +0200

    2/3 finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IProduct} from "./models/product";
-import {products as data} from './data/products'
+import {products, products as data} from './data/products'
+import {ProductService} from "./services/product.service";
+import {logMessages} from "@angular-devkit/build-angular/src/builders/browser-esbuild/esbuild";
+import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'main page'
+  // products: IProduct[] = []
+  loading: boolean
+  products$: Observable<IProduct[]>
+  term = ''
 
-  products: IProduct[]=data
+  constructor(private productsService: ProductService) {
+  }
+
+  ngOnInit(): void {
+    this.loading = true
+    this.products$ = this.productsService.getAll().pipe(
+      tap(() => this.loading = false)
+    )
+    // this.productsService.getAll().subscribe(products => {
+    //   this.products = products
+    //   this.loading = false
+    // })
+  }
+
+
 }
